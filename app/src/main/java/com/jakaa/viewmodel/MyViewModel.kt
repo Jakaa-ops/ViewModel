e8b8c93b27020ac462efeb6a67917074d05d3b2c
Jakaa <fredjakaa@gmail.com>
2023-09-16 10:05:40 +0300

    first commit

---
+++ app/src/main/java/com/jakaa/viewmodel/MyViewModel.kt
@@ -0,0 +1,17 @@
+package com.jakaa.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MyViewModel: ViewModel(){
+
+    private var counter = 0
+
+    fun getInitialCounter(): Int{
+        return counter
+    }
+
+    fun getUpdatedCounter(): Int{
+        return  ++counter
+    }
+
+}
